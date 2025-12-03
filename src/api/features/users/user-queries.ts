d0fc9a2ee05d545a63dba2e85114d6bf434434c3
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-03 14:08:21 +0000

    TSFP/Setup: Fix Tanstack Query setup by adding provider to component tree

--- src/api/features/users/user-queries.ts
+++ src/api/features/users/user-queries.ts
@@ -2,15 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "../../query-keys";
 import { getAllUsers, getUserById } from "./user-apis";
 
-export const useUsers = () => {
-  return useQuery({
+export const useUsers = <T> () => {
+  return useQuery<T>({
     queryKey: queryKeys.users,
     queryFn: getAllUsers,
   });
 };
 
-export const useUser = (id: string) => {
-  return useQuery({
+export const useUser = <T> (id: string) => {
+  return useQuery<T>({
     queryKey: queryKeys.users,
     queryFn: () => getUserById(id),
     enabled: !!id,
