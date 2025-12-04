139bffd9710582c6ae3d5f936ec1eab56ea3c01a
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-04 16:15:42 +0000

    TSFP-63 : Remove unused imports from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { useUsers } from "./api/features/users/user-queries";
 import "./App.css";
-import type { User } from "./types/mock";
 import { Login } from "./components/auth/Login";
 
 
