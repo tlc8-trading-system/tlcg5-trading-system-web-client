a6b8daf78256b2ee221ebffb020252d5a7b58398
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2025-12-04 17:30:49 +0000

    Revert "TSFP/Setup: Fix Tanstack Query setup by adding provider to component tree"
    This reverts commit 5df4e70ee817b578dbc1290a51448dbed1fa011c.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./api/query-client.ts";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
