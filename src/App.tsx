d9e0774044475b701c9eea739e739baeaf3a7b53
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2025-12-04 17:30:49 +0000

    Revert "TSFP/Setup: Install and set up tanstack query for server state management and exclude .env from git files"
    This reverts commit e13ca8b451ba0ba23d43ebeb02a38355ea86a098.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useUsers } from './api/features/users/user-queries'
 import './App.css'
 
 function App() {
-  const {data, isLoading, error} = useUsers();
-
-  if (isLoading) return <p>Loading...</p>
-  if (error) return <p>An error occured</p>
   return (
     <>
       <h1 className='text-5xl text-red-500'>Hello</h1>
-      <p>User list</p>
-      <div>
-        {data.map(() => {})} {/** Render all users */}
-      </div>
     </>
   )
 }
--- src/api/query-keys.ts
+++
@@ -1,5 +0,0 @@
-export const queryKeys = {
-    users: ["users"],
-    user: (id: string) => ["user", id],
-    // Add more keys as needed
-}
