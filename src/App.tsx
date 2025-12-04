3dedef1564d7880b6a3eb2218829bd566c6d5a35
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-04 10:42:12 +0000

    TSFP-75: Write Dashboard Pages layout to help with navigation to place order page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "sonner";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
+import { Layout } from "./components/Layout";
+import { Toaster } from "sonner";
+
+const DashboardPages = ({ children }: { children: React.ReactNode }) => {
+  return <Layout>{children}</Layout>;
+};
 
 function App() {
   return (
@@ -11,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={<DashboardPages>Dashboard Home</DashboardPages>}
+          />
         </Routes>
         <Toaster position="top-center" />
       </BrowserRouter>
