2c2e679bfa414affbb74f85490ddbfe06e25c5bf
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2025-12-04 17:30:49 +0000

    Revert "TSFP-63: Add login component to App.tsx"
    This reverts commit 4057367b6d161cc110df433296a29a0fafacf6ef.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 import { useUsers } from "./api/features/users/user-queries";
 import "./App.css";
 import type { User } from "./types/mock";
-import { Login } from "./components/auth/Login";
-
 
 function App() {
+  const { data, isLoading, error } = useUsers<User[]>();
+  const users = Array.isArray(data) ? data : [];
+
+  useEffect(() => {
+    //console.log("data: ", data);
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>An error occured</p>;
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Toaster position="top-center" />
-      </BrowserRouter>
+      <h1 className="text-5xl text-primary">Hello</h1>
+      <p>User list</p>
+      <div>
+        {users.map((user) => {
+          return <p>{user.name}</p>;
+        })}{" "}
+        {/** Render all users */}
+      </div>
     </>
   );
 }
