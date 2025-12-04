6802ce9657b3eb55a0ea74beba1d867940c61687
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-04 17:00:17 +0000

    TSFP-38: Navigate user to login page only after successful registration

--- src/api/features/authentication/auth-queries.ts
+++ src/api/features/authentication/auth-queries.ts
@@ -1,11 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { registerUser } from "./auth-apis";
 import { toast } from "sonner";
+import type { NavigateFunction } from "react-router-dom";
 
-export const RegisterUser = () => {
+export const RegisterUser = (navigate: NavigateFunction) => {
+  const onSuccess = () => {
+    toast("Registration successful, please log in");
+    navigate("/login");
+  };
+  
   return useMutation({
     mutationFn: registerUser,
-    onSuccess: () => toast("Registration successful, please log in"),
-    onError: () => toast("Registration failed, please try again")
+    onSuccess,
+    onError: () => toast("Registration failed, please try again"),
   });
 };
