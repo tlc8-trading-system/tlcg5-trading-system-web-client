afe012b6d9085f3dc391b1047d1bb0ba4b123a1d
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-05 13:52:38 +0000

    TSFP-83/Feat: Develop frontend logic to display pending orders ready for backend integration

--- src/api/features/orders/order-api.ts
+++ src/api/features/orders/order-api.ts
@@ -4,3 +4,8 @@ import apiClient from "../../api-client";
 export const placeOrder = async (orderDetails: PlaceOrderRequest) => {
   await apiClient.post("place-order-endpoint", orderDetails);
 };
+
+export const fetchPendingOrders = async () => {
+  const {data} = await apiClient.get("pending-orders-api");
+  return data
+};
--- src/api/features/orders/order-queries.ts
+++ src/api/features/orders/order-queries.ts
@@ -1,7 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
-import { placeOrder } from "./order-api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { fetchPendingOrders, placeOrder } from "./order-api";
 import { toast } from "sonner";
 import type { NavigateFunction } from "react-router-dom";
+import { queryKeys } from "../../query-keys";
+import type { ServerResponse } from "../../../types/server";
+import type { Order } from "../../../types";
 
 export const PlaceOrder = (
   orderDescription: string,
@@ -18,5 +21,12 @@ export const PlaceOrder = (
     mutationFn: placeOrder,
     onSuccess,
     onError: () => toast("Unable to place order, please try again later"),
+  });
+};
+
+export const usePendingOrders = () => {
+  return useQuery<ServerResponse<Order>>({
+    queryKey: queryKeys.pendingOrders,
+    queryFn: fetchPendingOrders,
   });
 };
