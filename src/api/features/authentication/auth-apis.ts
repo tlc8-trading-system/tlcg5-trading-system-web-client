2fd70a1b82b7f0128f34716a15ffb9c61470d457
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-06 00:32:45 +0000

    TSFP-62: Add loginUser function to handle user login requests

--- src/api/features/authentication/auth-apis.ts
+++ src/api/features/authentication/auth-apis.ts
@@ -1,6 +1,11 @@
-import type { RegisterRequest } from "../../../types";
+import type { LoginRequest, LoginResponse, RegisterRequest } from "../../../types";
 import apiClient from "../../api-client";
 
 export const registerUser = async (user: RegisterRequest) => {
   await apiClient.post("register-endpoint", user);
 };
+
+export const loginUser = async (credentials:LoginRequest): Promise<LoginResponse> => {
+  const response = await apiClient.post("login-endpoint", credentials);
+  return response.data;
+}
