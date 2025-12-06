87bea2d8e8fbee03ce884ab91db32c5bfcb21a36
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-06 00:39:01 +0000

    TSFP-62: Add AuthProvider component for user authentication management

---
+++ src/providers/auth-providers.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import type { User } from "../types";
+import AuthContext from "../contexts/auth-context";
+import { mockUser } from "../data/mock-data";
+import { toast } from "sonner";
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const getNewAccessToken = () => {
+    return "new-access-token"; 
+  };
+
+  const login = (user: User, accessToken: string) => {
+    setUser(mockUser);
+  
+  sessionStorage.setItem("user", JSON.stringify(user));
+  sessionStorage.setItem("access-token", accessToken);
+  };
+
+  const logout = () => {
+    setUser(null);
+    sessionStorage.clear()
+  };
+
+  useEffect(() => {
+    
+    const fetchAlreadyLoggedInUser = () => {
+      const browserUser = sessionStorage.getItem("user");
+      let accessToken = sessionStorage.getItem("access-token");
+      if (browserUser && accessToken) {
+        setUser(JSON.parse(browserUser));
+      } else {
+        const newAccessToken = getNewAccessToken();
+        if (!newAccessToken) {
+          toast("You must log in again");
+          logout();
+        }
+        accessToken = newAccessToken;
+      }
+    };
+
+    fetchAlreadyLoggedInUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+        isAuthenticated: !!user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
