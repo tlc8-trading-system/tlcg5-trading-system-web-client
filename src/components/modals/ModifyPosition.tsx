8e5d4e5294aab1ceb305d6798436123ef060026b
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-08 15:59:43 +0000

    TSFP-117/Feat: Display Trade Details on Modification Screen

--- src/components/modals/ModifyPosition.tsx
+++ src/components/modals/ModifyPosition.tsx
@@ -32,7 +32,7 @@ const ModifyPosition: React.FC<ModifyPositionProps> = ({
     <Dialog open={showModifyDialog} onOpenChange={setShowModifyDialog}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Modify Position</DialogTitle>
+          <DialogTitle>Modify your trade</DialogTitle>
           <DialogDescription>
             Adjust stop loss and take profit for your {modifyData?.symbol}
           </DialogDescription>
@@ -41,41 +41,73 @@ const ModifyPosition: React.FC<ModifyPositionProps> = ({
         {modifyData && (
           <div className="space-y-4 py-4">
             <div className="space-y-2">
-              <Label htmlFor="stopLoss">Stop Loss</Label>
-              <Input
-                id="stopLoss"
-                type="number"
-                step="0.01"
-                value={modifyData.stopLoss}
-                onChange={(e) => {
-                  const val = Number(e.target.value);
-                  if (
-                    (trade.type == "Buy" && val > trade.entryPrice) ||
-                    (trade.type === "Sell" && val < trade.entryPrice)
-                  )
-                    return;
-                  setModifyData({ ...modifyData, stopLoss: val.toString() });
-                }}
-              />
+              <Label htmlFor="symbol">Asset</Label>
+              <Input id="symbol" type="text" value={trade.symbol} disabled />
+            </div>
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="quantity">
+                  Quantity {trade.type === "Buy" ? "bought" : "sold"}
+                </Label>
+                <Input
+                  id="quantity"
+                  type="text"
+                  value={trade.quantity}
+                  disabled
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="entryPrice">Entry Price</Label>
+                <Input
+                  id="entryPrice"
+                  type="number"
+                  value={trade.entryPrice}
+                  disabled
+                />
+              </div>
             </div>
 
-            <div className="space-y-2">
-              <Label htmlFor="takeProfit">Take Profit</Label>
-              <Input
-                id="takeProfit"
-                type="number"
-                step="0.01"
-                value={modifyData.takeProfit}
-                onChange={(e) => {
-                  const val = Number(e.target.value);
-                  if (
-                    (trade.type == "Sell" && val > trade.entryPrice) ||
-                    (trade.type == "Buy" && val < trade.entryPrice)
-                  )
-                    return;
-                  setModifyData({ ...modifyData, takeProfit: val.toString() });
-                }}
-              />
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-2">
+                <Label htmlFor="stopLoss">Stop Loss</Label>
+                <Input
+                  id="stopLoss"
+                  type="number"
+                  step="0.01"
+                  value={modifyData.stopLoss}
+                  onChange={(e) => {
+                    const val = Number(e.target.value);
+                    if (
+                      (trade.type == "Buy" && val > trade.entryPrice) ||
+                      (trade.type === "Sell" && val < trade.entryPrice)
+                    )
+                      return;
+                    setModifyData({ ...modifyData, stopLoss: val.toString() });
+                  }}
+                />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="takeProfit">Take Profit</Label>
+                <Input
+                  id="takeProfit"
+                  type="number"
+                  step="0.01"
+                  value={modifyData.takeProfit}
+                  onChange={(e) => {
+                    const val = Number(e.target.value);
+                    if (
+                      (trade.type == "Sell" && val > trade.entryPrice) ||
+                      (trade.type == "Buy" && val < trade.entryPrice)
+                    )
+                      return;
+                    setModifyData({
+                      ...modifyData,
+                      takeProfit: val.toString(),
+                    });
+                  }}
+                />
+              </div>
             </div>
 
             <div className="rounded-lg bg-muted p-3 text-sm text-muted-foreground">
