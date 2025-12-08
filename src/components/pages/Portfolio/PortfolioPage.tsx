d3d6ebf6e2d1de5cb9b581ea5fe18873d69ba0fb
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-08 20:33:45 +0000

    TSFP-86: Add PortfolioPage component to pages

---
+++ src/components/pages/Portfolio/PortfolioPage.tsx
@@ -0,0 +1,29 @@
+import { Plus } from "lucide-react";
+import { PortfolioList } from "../../portfolio/Portfolios";
+import DashboardPagesHeader from "../../shared/dashboard-pages-header";
+import { Button } from "../../ui/button";
+import { useNavigate } from "react-router-dom";
+
+export function PortfolioPage() {
+  const navigate = useNavigate();
+  return (
+    <div className="space-y-8 w-full">
+      <div className="flex items-center justify-between">
+        <DashboardPagesHeader
+        pageTitle="Portfolios"
+        pageDescription="Manage your investment portfolios"
+      />
+
+      <Button onClick={() => navigate('/portfolio/create')} className="h-10">
+            <Plus className="size-4 mr-2" />
+            Create Portfolio
+        </Button>
+
+
+      
+      </div>
+
+      <PortfolioList />
+    </div>
+  );
+}
