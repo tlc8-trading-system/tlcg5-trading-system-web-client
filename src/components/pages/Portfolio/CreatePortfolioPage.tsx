514f852941527526eec3b61262465baf24c200e3
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-09 13:51:03 +0000

    TSFP-129/Feat: Create CreatePortfolioPage component for portfolio setup

---
+++ src/components/pages/Portfolio/CreatePortfolioPage.tsx
@@ -0,0 +1,17 @@
+import DashboardPagesHeader from "../../shared/dashboard-pages-header";
+import { CreatePortfolio } from "../../portfolio/CreatePortfolio";
+
+
+export function CreatePortfolioPage() {
+
+  return (
+    <div className="space-y-8 w-full">
+        <DashboardPagesHeader
+        pageTitle="Create Portfolio"
+        pageDescription="Set up a new investment portfolio"
+        />
+        <CreatePortfolio/>
+    </div>
+
+  );
+}
