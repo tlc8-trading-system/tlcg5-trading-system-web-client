e3b54d76e72d8fc2c82be8a8a4415d1b8a650932
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-09 22:19:54 +0000

    TSFP-64/Feat: Add login endpoint to authEndpoints

--- src/api/api-endpoints.ts
+++ src/api/api-endpoints.ts
@@ -2,6 +2,7 @@ const baseUrl = import.meta.env.VITE_APP_SERVER_BASE_URL;
 
 export const endpoints = {
     authEndpoints: {
-        register: baseUrl + "/auth/register"
+        register: baseUrl + "/auth/register",
+        login: baseUrl + "/auth/login"
     }
 }
