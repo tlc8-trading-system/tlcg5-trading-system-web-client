06c4eacb07d379a084ff975022979ead95ad1637
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-10 12:30:23 +0000

    TSFP-64/Refactor: Add withCredentials option to axios instance

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -4,7 +4,8 @@ const apiClient = axios.create({
     baseURL: import.meta.env.VITE_APP_SERVER_BASE_URL,
     headers: {
         "Content-Type": "application/json"
-    }
+    },
+    withCredentials: true,
 })
 
 export default apiClient;
