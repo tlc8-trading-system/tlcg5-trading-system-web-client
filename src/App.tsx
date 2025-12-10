a1341d89ea5daaa7a2f6a306a9c27ec58a762157
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-10 15:12:57 +0000

    TSFP-64/Refactor: Remove Navigate from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Toaster } from "sonner";
 import { Login } from "./components/auth/Login";
