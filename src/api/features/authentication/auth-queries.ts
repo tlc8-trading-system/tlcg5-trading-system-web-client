0b7bcac326b4b9ff931a14b4d88eddf51486e0b6
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-10 12:32:48 +0000

    TSFP-64/Refactor: Update LoginUser mutation to use async onSuccess and invalidate queries

--- src/api/features/authentication/auth-queries.ts
+++ src/api/features/authentication/auth-queries.ts
@@ -3,6 +3,7 @@ import { loginUser, registerUser } from "./auth-apis";
 import { toast } from "sonner";
 import type { NavigateFunction } from "react-router-dom";
 import type { ApiResponse, LoginRequest, LoginResponse } from "../../../types";
+import queryClient from "../../query-client";
 
 export const RegisterUser = (navigate: NavigateFunction) => {
   const onSuccess = () => {
@@ -22,8 +23,7 @@ export const RegisterUser = (navigate: NavigateFunction) => {
 export const LoginUser = (navigate: NavigateFunction) => {
   return useMutation<ApiResponse<LoginResponse>, Error, LoginRequest>({
     mutationFn: loginUser,
-
-    onSuccess: (res) => {
+    onSuccess: async(res) => {
       if (!res.data) {
         toast.error(res.message);
         return;
@@ -31,6 +31,8 @@ export const LoginUser = (navigate: NavigateFunction) => {
 
       toast.success(`Welcome back, ${res.data.firstname}!`);
       navigate("/dashboard");
+
+      await queryClient.invalidateQueries({ queryKey: ["me"] });
     },
 
     onError: (error) => {
