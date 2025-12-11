73e063aa0e4e547416de5ee5549e83db1eba5b33
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-11 22:17:17 +0000

    TSFP-176/Feature: Add mock performance data

---
+++ src/data/mock-performance.ts
@@ -0,0 +1,9 @@
+  export const performanceData = [
+    { date: '11/25', value: 100 },
+    { date: '11/26', value: 101 },
+    { date: '11/27', value: 120 },
+    { date: '11/28', value: 125 },
+    { date: '11/29', value: 130 },
+    { date: '11/30', value: 135},
+    { date: '12/01', value: 140 },
+  ];
