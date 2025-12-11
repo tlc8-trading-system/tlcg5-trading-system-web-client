693de1cf8b185086bea83a2fd37b82126aa223a0
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-11 12:33:38 +0000

    TSFP-165/Refactor: Update query key for portfolios

--- src/api/features/portfolios/portfolio-queries.ts
+++ src/api/features/portfolios/portfolio-queries.ts
@@ -1,12 +1,12 @@
 import { useQuery} from "@tanstack/react-query";
-import { portfolioKeys } from "../../query-keys"; 
+import { queryKeys } from "../../query-keys"; 
 import { fetchPortfolios} from "./portfolio-apis"; 
 import type { Portfolio } from "../../../types";
 
 
 export const usePortfoliosQuery = () => {
     return useQuery<Portfolio[]>({
-        queryKey: portfolioKeys.all,
+        queryKey: queryKeys.portfolios,
         queryFn: fetchPortfolios,
         staleTime: 1000 * 60 * 5, 
     });
