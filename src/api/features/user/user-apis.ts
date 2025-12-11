958ab9d14084ec4a177b73f153be0f15f8db56ae
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-11 12:34:12 +0000

    TSFP-165/Refactor: Remove User type assertion

--- src/api/features/user/user-apis.ts
+++ src/api/features/user/user-apis.ts
@@ -1,8 +1,7 @@
-import type { User } from "../../../types";
 import apiClient from "../../api-client"
 import { endpoints } from "../../api-endpoints";
 
 export const getUser = async () => {
     const response = await apiClient.get(endpoints.userEndpoints.me);
-    return response.data.data as User;
+    return response.data.data;
 };
