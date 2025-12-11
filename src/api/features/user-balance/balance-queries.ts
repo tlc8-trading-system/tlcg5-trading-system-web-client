4f96c106627a811ad97644ba1b6240e23c78db92
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-11 12:04:50 +0000

    TSFP-70/Refactor: Modify type to fit incoming balance structure from API

--- src/api/features/user-balance/balance-queries.ts
+++ src/api/features/user-balance/balance-queries.ts
@@ -3,8 +3,12 @@ import { queryKeys } from "../../query-keys";
 import { getUserBalance } from "./balance-api";
 import type { ServerResponse } from "../../../types/server";
 
+interface balanceReponse {
+  balance: number
+}
+
 export const useUserBalance = () => {
-  return useQuery<ServerResponse<number>>({
+  return useQuery<ServerResponse<balanceReponse>>({
     queryKey: queryKeys.balance,
     queryFn: getUserBalance,
   });
