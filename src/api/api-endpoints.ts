ef2d4989ba2f0b25d8628b8a42facd191ceeeacc
samuella.aglago <samuella.aglago@turntabl.io>
2025-12-11 11:56:13 +0000

    TSFP-70/Feat: Get and display user balance

--- src/api/api-endpoints.ts
+++ src/api/api-endpoints.ts
@@ -2,7 +2,7 @@ const baseUrl = import.meta.env.VITE_APP_SERVER_BASE_URL;
 
 export const endpoints = {
     authEndpoints: {
-        register: baseUrl + "api/auth/register",
+        register: baseUrl + "/api/auth/register",
         login: baseUrl + "/api/auth/login",
         logout: baseUrl + "/api/auth/logout"
 
@@ -11,7 +11,8 @@ export const endpoints = {
         me: baseUrl + "/api/user/me"
     },
     tradeEndpoints: {
-        allTrades: baseUrl + "/api/order/open"
+        allTrades: baseUrl + "/api/order/open",
+        balance: baseUrl + "/api/client/balance"
     },
     orderEndpoints: {
         allPendingOrders: baseUrl + "/api/order"
