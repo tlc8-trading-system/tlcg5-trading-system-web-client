53d9d7bfde80c43375ab8b079554d15e1eda3aba
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-12 16:54:35 +0000

    TSFP-176/Feat: Implement usePortfoliosQuery and usePortfolioDetailsQuery

--- src/api/features/portfolios/portfolio-queries.ts
+++ src/api/features/portfolios/portfolio-queries.ts
@@ -1,12 +1,13 @@
 import { useMutation, useQuery} from "@tanstack/react-query";
 import { queryKeys } from "../../query-keys"; 
-import { createPortfolio, fetchPortfolios} from "./portfolio-apis"; 
-import type {Portfolio } from "../../../types";
+import { createPortfolio, fetchPortfolioDetails, fetchPortfolios} from "./portfolio-apis"; 
+import type { ApiResponse, Portfolio,PortfolioDetails } from "../../../types";
 import { toast } from "sonner";
+import { useQuery } from "@tanstack/react-query";
 
 
 export const usePortfoliosQuery = () => {
-    return useQuery<Portfolio[]>({
+    return useQuery<ApiResponse<Portfolio[]>>({
         queryKey: queryKeys.portfolios,
         queryFn: fetchPortfolios,
         staleTime: 1000 * 60 * 5, 
@@ -25,6 +26,14 @@ export const CreatePortfolio =  () => {
         onError: () => toast("Failed to create portfolio")
     })
 }
+
+export const usePortfolioDetailsQuery = () => {
+    return useQuery<ApiResponse< PortfolioDetails[]>>({
+        queryKey: queryKeys.portfolioDetails,
+        queryFn: fetchPortfolioDetails,
+        staleTime: 1000 * 60 * 5, 
+    });
+};
 
 
 
