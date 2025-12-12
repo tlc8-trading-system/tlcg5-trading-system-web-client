af5844920a04aced7e1f3f71820e8c3308e23fd5
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-12 08:48:34 +0000

    TSFP-87/Feat: Implement CreatePortfolio mutation

--- src/api/features/portfolios/portfolio-queries.ts
+++ src/api/features/portfolios/portfolio-queries.ts
@@ -1,7 +1,8 @@
-import { useQuery} from "@tanstack/react-query";
+import { useMutation, useQuery} from "@tanstack/react-query";
 import { queryKeys } from "../../query-keys"; 
-import { fetchPortfolios} from "./portfolio-apis"; 
-import type { Portfolio } from "../../../types";
+import { createPortfolio, fetchPortfolios} from "./portfolio-apis"; 
+import type {Portfolio } from "../../../types";
+import { toast } from "sonner";
 
 
 export const usePortfoliosQuery = () => {
@@ -11,6 +12,19 @@ export const usePortfoliosQuery = () => {
         staleTime: 1000 * 60 * 5, 
     });
 };
+
+
+export const CreatePortfolio =  () => {
+    const onSuccess = () =>{
+        toast("Porfolio created successfully");
+    };
+
+    return useMutation({
+        mutationFn: createPortfolio,
+        onSuccess,
+        onError: () => toast("Failed to create portfolio")
+    })
+}
 
 
 
