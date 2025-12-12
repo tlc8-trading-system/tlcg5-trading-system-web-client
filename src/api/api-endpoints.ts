800fd4bfc80d2ca7c32ed45a3a70efd80dc8cdf5
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-12 15:54:37 +0000

    TSFP-193/Feat: Add trade history endpoint

--- src/api/api-endpoints.ts
+++ src/api/api-endpoints.ts
@@ -13,6 +13,7 @@ export const endpoints = {
   },
   tradeEndpoints: {
     allTrades: baseUrl + "/api/order/open",
+        tradeHistory: baseUrl + "/api/order/history",
   },
   orderEndpoints: {
     allPendingOrders: baseUrl + "/api/order",
