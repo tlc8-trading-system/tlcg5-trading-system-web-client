681054fb1b2729315fb85c014c4e317d7320fbf6
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-12 08:40:20 +0000

    TSFP-87/Refactor : Add createPorftolio endpoint

--- src/api/features/portfolios/portfolio-apis.ts
+++ src/api/features/portfolios/portfolio-apis.ts
@@ -1,5 +1,7 @@
 import { mockPortfolios } from "../../../data/mock-portfolios";
-import type { Portfolio } from "../../../types";
+import type { CreateNewPortfolio, Portfolio } from "../../../types";
+import apiClient from "../../api-client";
+import { endpoints } from "../../api-endpoints";
 let dbPortfolios = [...mockPortfolios]; 
 
 
@@ -7,3 +9,6 @@ export const fetchPortfolios = async (): Promise<Portfolio[]> => {
   return dbPortfolios;
 };
 
+export const createPortfolio = async (newPortfolio:CreateNewPortfolio ) => {
+  apiClient.post(endpoints.postfolioEndpoints.createPortfolio, newPortfolio)
+}
