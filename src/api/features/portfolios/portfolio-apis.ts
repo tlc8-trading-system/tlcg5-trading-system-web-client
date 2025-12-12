67357775d5b25521c56388bad59e1c6472fdec1e
Michael Adu-Gyamfi <michaelaugyamfi76@gmail.com>
2025-12-12 16:53:58 +0000

    TSFP-176/Feat : Define fetchPortolios and fetchPortfolios api call

--- src/api/features/portfolios/portfolio-apis.ts
+++ src/api/features/portfolios/portfolio-apis.ts
@@ -1,14 +1,13 @@
-import { mockPortfolios } from "../../../data/mock-portfolios";
 import type { CreateNewPortfolio, Portfolio } from "../../../types";
 import apiClient from "../../api-client";
 import { endpoints } from "../../api-endpoints";
-const dbPortfolios = [...mockPortfolios]; 
 
 
-export const fetchPortfolios = async (): Promise<Portfolio[]> => {
-  return dbPortfolios;
+
+export const fetchPortfolioDetails = async () => {
+  const {data} =  await apiClient.get(endpoints.portfolioEndpoints.fetchPortfolioDetails);
+  return data;
 };
-
 export const createPortfolio = async (newPortfolio:CreateNewPortfolio ) => {
   apiClient.post(endpoints.portfolioEndpoints.createPortfolio, newPortfolio)
 }
